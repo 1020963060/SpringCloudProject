4f43608763f083de1f1aa690d4b1d413cd425eb2
1020963060 <1020963060@qq.com>
2018-09-28 17:34:43 +0800

    springcould的练习

---
+++ src/main/java/com/qq/springcloud/SpringcloudApplication.java
@@ -0,0 +1,14 @@
+package com.qq.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer//这是一个注册中心
+public class SpringcloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudApplication.class, args);
+    }
+}
